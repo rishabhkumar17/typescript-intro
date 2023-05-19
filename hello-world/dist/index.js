c08e63475d89e6270cbaf2043f7d114f581e9405
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-05-19 13:11:46 +0530

    any-type

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,6 +1,9 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+function render(document) {
+    console.log(document);
+}
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,5 +1,14 @@
 // console.log("Hello World!");
 
-let age: number = 20;
-if (age < 50) age += 10;
-console.log(age);
+// let age: number = 20;
+// if (age < 50) age += 10;
+// console.log(age);
+
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+let level; // any type
+
+function render(document: any) {
+  console.log(document);
+}
