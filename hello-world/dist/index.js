ca7cb6ecf497056bfe80f4e9aaf19169f88372c9
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-05-21 19:13:31 +0530

    enums

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -9,4 +9,14 @@ function render(document) {
 let numbers = [1, 2, 3];
 let user = [1, "sage"];
 user.push(1);
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
+let mysize = 2;
+console.log(mysize);
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -19,3 +19,28 @@ let numbers: number[] = [1, 2, 3];
 // TUPLES
 let user: [number, string] = [1, "sage"];
 user.push(1); // gaps in typescript
+
+// ENUMS
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+// PascalCase
+enum Size {
+  Small = 1,
+  Medium,
+  Large,
+} // Explicitly set values = 1, 2, 3
+
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
+// Adding const produces optimized code
+const enum size {
+  Small = 1,
+  Medium,
+  Large,
+} // Explicitly set values = 1, 2, 3
+
+let mysize: size = size.Medium;
+console.log(mysize);
