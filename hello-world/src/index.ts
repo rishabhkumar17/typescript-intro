df199b0987db0062b749d069ff9edd972379a37f
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-05-25 19:37:09 +0530

    optional-property-access-operator

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -115,3 +115,19 @@ function greet(name: string | null | undefined) {
 
 greet(null);
 greet(undefined);
+
+// OPTIONAL CHAINING
+type Customer = {
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(0);
+// if (customer !== null && customer !== undefined)
+
+// Optional property access operator
+
+console.log(customer?.birthday?.getFullYear());
